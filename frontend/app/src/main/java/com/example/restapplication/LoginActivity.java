00bd65a52993281924491bf2adb5b374c71812f7
IdrisLokhande <73928640+IdrisLokhande@users.noreply.github.com>
2025-11-06 02:08:00 +0530

    Corrected Indentation

--- frontend/app/src/main/java/com/example/restapplication/LoginActivity.java
+++ frontend/app/src/main/java/com/example/restapplication/LoginActivity.java
@@ -31,73 +31,74 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-	// Set up SessionManager
-	session = new SessionManager(LoginActivity.this);
+		// Set up SessionManager
+		session = new SessionManager(LoginActivity.this);
 
         // Inflate the layout
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         
-	// Set custom password mask
-	EditText editPassword = binding.password;
+		// Set custom password mask
+		EditText editPassword = binding.password;
         editPassword.setTransformationMethod(new CustomPasswordTransformation());
         editPassword.setOnFocusChangeListener((v, hasFocus) -> {
-		CustomPasswordTransformation newTm = new CustomPasswordTransformation();
-		editPassword.setTransformationMethod(newTm);
-		if(!hasFocus){
-			newTm.toggleLastCharMask(true);	
-		}else{
-			newTm.toggleLastCharMask(false);
-		}
-	});
+			CustomPasswordTransformation newTm = new CustomPasswordTransformation();
+			editPassword.setTransformationMethod(newTm);
+			if(!hasFocus){
+				newTm.toggleLastCharMask(true);	
+			}else{
+				newTm.toggleLastCharMask(false);
+			}
+		});
 
-	// For Register
-	binding.textRegister.setOnClickListener(v -> {
+		// For Register
+		binding.textRegister.setOnClickListener(v -> {
     		Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
     		startActivity(intent);
-	});
+		});
 
-	// For Freepik URL Linking
-	binding.textFreepik.setMovementMethod(LinkMovementMethod.getInstance());
+		// For Freepik URL Linking
+		binding.textFreepik.setMovementMethod(LinkMovementMethod.getInstance());
 
         // Set up login button click listener
         binding.loginButton.setOnClickListener(v -> {
-	    binding.password.clearFocus();	    
+	    	binding.password.clearFocus();	    
             binding.username.requestFocus();	
 	
             // Authentication logic in server.js
-	    String username = binding.username.getText().toString().trim();
+	    	String username = binding.username.getText().toString().trim();
     	    String password = binding.password.getText().toString().trim();
 
             User user = new User(username, password);
             APIService api = RetrofitClient.getInstance().create(APIService.class);
 
-	    api.loginUser(user).enqueue(new Callback<LoRResponse>() {
-	    	@Override
-                public void onResponse(Call<LoRResponse> call, Response<LoRResponse> response){
-                	if(response.isSuccessful() && response.body() != null && response.body().isSuccess()){
-				session.saveUser(username, response.body().getUserId());
-				// Navigate to MainActivity
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish(); // Prevent going back to login screen
-			}else if(response.body() != null){
-				Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
-			}
-                }
+	    	api.loginUser(user).enqueue(new Callback<LoRResponse>() {
+	    			@Override
+                	public void onResponse(Call<LoRResponse> call, Response<LoRResponse> response){
+                		if(response.isSuccessful() && response.body() != null && response.body().isSuccess()){
+							session.saveUser(username, response.body().getUserId());
+							// Navigate to MainActivity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); // Prevent going back to login screen
+						}else if(response.body() != null){
+							Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+						}
+                	}
  
-                @Override
-                public void onFailure(Call<LoRResponse> call, Throwable t){
-                	Toast.makeText(LoginActivity.this, "Network Error!", Toast.LENGTH_SHORT).show();
+                	@Override
+                	public void onFailure(Call<LoRResponse> call, Throwable t){
+                		Toast.makeText(LoginActivity.this, "Network Error!", Toast.LENGTH_SHORT).show();
                         Log.e("Retrofit", "onFailure: ", t);
-                }
+                	}
             });
         });
     }
 
     @Override
     protected void onDestroy(){
-	super.onDestroy();
-	binding = null;
+		super.onDestroy();
+		binding = null;
+		session = null;
     }
 }
