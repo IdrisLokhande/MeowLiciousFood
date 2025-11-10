304cce5c9d907a888dac0c4752ae6723a9d71e70
IdrisLokhande <73928640+IdrisLokhande@users.noreply.github.com>
2025-11-11 00:55:01 +0530

    Add Gson annotations to User class fields
    Added Gson annotations for JSON serialization.

--- frontend/app/src/main/java/com/example/restapplication/backendlink/User.java
+++ frontend/app/src/main/java/com/example/restapplication/backendlink/User.java
@@ -1,8 +1,13 @@
 package com.example.restapplication.backendlink;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+	@SerializedName("uid")
     private int uid;
+	@SerializedName("username")
     private String username;
+	@SerializedName("password")
     private String password;
 
     public User(String username, String password) {
