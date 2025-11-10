6e094985de4d82092f6b26578b7bfe76c8d39e55
IdrisLokhande <73928640+IdrisLokhande@users.noreply.github.com>
2025-11-11 00:51:53 +0530

    Enhance RegisterUser class with serializednames
    Added fields for user registration with Gson annotations.

--- frontend/app/src/main/java/com/example/restapplication/backendlink/RegisterUser.java
+++ frontend/app/src/main/java/com/example/restapplication/backendlink/RegisterUser.java
@@ -1,12 +1,21 @@
 package com.example.restapplication.backendlink;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RegisterUser {
+    @SerializedName("email")
     private String email;
+    @SerializedName("username")
     private String username;
+    @SerializedName("fname")
     private String firstName;
+    @SerializedName("lname")
     private String lastName;
+    @SerializedName("phone")
     private String phone;
+    @SerializedName("address")
     private String address;
+    @SerializedName("password")
     private String password;
 
     public RegisterUser(String email, String username, String firstName, String lastName, String phone, String address, String password) {
