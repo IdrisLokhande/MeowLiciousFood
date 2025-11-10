9e67e4d1ac4c4181fb4f34cbe814f2961bac0e00
IdrisLokhande <73928640+IdrisLokhande@users.noreply.github.com>
2025-11-11 00:49:09 +0530

    Clarify uid field type in LoRResponse
    Added comment to clarify the use of Integer for uid.

--- frontend/app/src/main/java/com/example/restapplication/backendlink/LoRResponse.java
+++ frontend/app/src/main/java/com/example/restapplication/backendlink/LoRResponse.java
@@ -1,12 +1,12 @@
 package com.example.restapplication.backendlink;
 
-
+import com.google.gson.annotations.SerializedName;
 
 public class LoRResponse {
     private boolean success;
     private String message;
 	@SerializedName("uid")
-    private Integer uid;
+    private Integer uid; // not int because configured to return null in some cases
 
     public boolean isSuccess() {
         return success;
